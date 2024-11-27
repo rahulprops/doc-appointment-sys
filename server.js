ae1ebc04901a974fb627bed3129bc94963248a46
rahulprops <rahulhack912@gmail.com>
2024-11-27 10:40:03 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import 'dotenv/config'
+const app=express()
+const port= process.env.PORT;
+
+
+app.get("/api" , async (req,res)=>{
+console.log("hwllo")
+})
+app.listen(port,()=>{
+    console.log(`server is running or port http://localhost:${port}`)
+})
